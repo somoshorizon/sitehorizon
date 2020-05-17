79989e4022df9be35c7410e8845e79eaeffc7f97
somoshorizon <64874900+somoshorizon@users.noreply.github.com>
2020-05-17 03:01:30 -0300

    Add files via upload

---
+++ js/js/particleapp.js
@@ -0,0 +1,112 @@
+particlesJS(
+    {
+        "particles": {
+          "number": {
+            "value": 250,
+            "density": {
+              "enable": true,
+              "value_area": 3126.65351868777
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#e30000"
+            },
+            "polygon": {
+              "nb_sides": 3
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1.7865698670629593,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 0.1,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 17.053621458328248,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 100,
+            "color": "#ffffff",
+            "opacity": 1,
+            "width": 2.0844356791251797
+          },
+          "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "top-right",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "window",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 3,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": false
+      }
+)
